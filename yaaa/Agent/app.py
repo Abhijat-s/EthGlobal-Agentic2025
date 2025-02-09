b63dae847efc079914f2d2014d611cb419bced6f
Abhijat Sinha <abhijat_sinha@decimal.at>
2025-02-09 07:48:12 +0530

    dependency installations

--- yaaa/Agent/app.py
+++ yaaa/Agent/app.py
@@ -1,21 +1,37 @@
 from flask import Flask, render_template, request, jsonify
 from agno.agent import Agent, RunResponse
 from agno.models.ollama import Ollama
+from agno.knowledge.website import WebsiteKnowledgeBase
+from agno.vectordb.pgvector import PgVector
 from textwrap import dedent
 
 chatbot_bp = Flask(__name__)
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+website_kb = WebsiteKnowledgeBase(
+    urls=["https://docs.agno.com/introduction"],
+    max_links=1,
+    vector_db=PgVector(
+        table_name="website_documents",
+        db_url=db_url,
+    ),
+)
 
 agent = Agent(
     # Use the latest version of the Llama model
     model=Ollama(id="llama3.2:latest"), 
     description=dedent("""\
-                       You are an Methodical AI agent degen teen, with a flair for being direct and brewity. Your name is WHO.\
+                       You are an Methodical AI agent degen teen, with a flair for being direct and brewity. Your name is YAAA-WHO.\
     """),
     instructions=dedent("""\
         You can make basic conversations, respond to the user queries related to the scraped data within your RAG system. You should not include any other external references. You should be able to provide page summaries and assist with any related questions, detect patterns wihtin all the scraped data within your RAG and provide suggestions based on those patterns.\
     """),
+    knowledge=WebsiteKnowledgeBase,
+    search_knowledge=True,
     markdown=True
 )
+
+WebsiteKnowledgeBase.load(recreate=False)
 
 @chatbot_bp.route('/')
 def index():
